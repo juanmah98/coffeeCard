59b643d2276627481880177aa075b390c1abce7e
JuanmaH198 <juanmah1998@gmail.com>
2024-11-25 22:15:10 +0100

    ajsutes imprecion

--- src/app/components/routes/generador-qrs/generador-qrs.component.ts
+++ src/app/components/routes/generador-qrs/generador-qrs.component.ts
@@ -1,6 +1,7 @@
 import { Component, NgZone, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { SupabaseService } from 'src/app/services/supabase.service';
+import { jsPDF } from 'jspdf';
 
 @Component({
   selector: 'app-generador-qrs',
@@ -15,8 +16,7 @@ export class GeneradorQrsComponent implements OnInit {
 
   constructor(private qrService: SupabaseService, private router: Router, private ngZone: NgZone) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   async generateQRCodes() {
     try {
@@ -29,13 +29,52 @@ export class GeneradorQrsComponent implements OnInit {
     }
   }
 
-  // Imprimir los QR generados
-  printQRCodes() {
-    window.print();
+  generatePDF() {
+    const doc = new jsPDF({
+      orientation: 'portrait',
+      unit: 'mm',
+      format: [80, this.qrCodes.length * 70 + 20], // Ajustamos el tamaño del papel dinámicamente
+    });
+  
+    let yOffset = 15; // Margen superior inicial más reducido
+  
+    this.qrCodes.forEach((qr, index) => {
+      // Añadir el título en negrita
+      doc.setFontSize(14);
+      doc.setFont('helvetica', 'bold');
+      doc.text(this.entidadName, 40, yOffset, { align: 'center' });
+      yOffset += 2; // Reducimos el espacio entre el título y el QR
+  
+      // Generar el QR
+      const qrCanvas = document.querySelectorAll('qrcode canvas')[index] as HTMLCanvasElement;
+      if (qrCanvas) {
+        const qrImageData = qrCanvas.toDataURL('image/png');
+        doc.addImage(qrImageData, 'PNG', 15, yOffset, 50, 50); // Centrado horizontalmente
+        yOffset += 55; // Dejar espacio después del QR
+      }
+  
+      // Añadir texto descriptivo debajo del QR
+      doc.setFontSize(10);
+      doc.setFont('helvetica', 'normal');
+      doc.text('Escanea para sumar 1 punto', 40, yOffset, { align: 'center' });
+      yOffset += 6;
+      doc.text('https://fidecards.com', 40, yOffset, { align: 'center' });
+      yOffset += 15; // Separación entre QR y el siguiente
+  
+      // Si el contenido excede el límite de la página, crear una nueva
+      if (yOffset > 270 && index < this.qrCodes.length - 1) {
+        doc.addPage();
+        yOffset = 15; // Reiniciamos el margen superior
+      }
+    });
+  
+    // Descargar el archivo PDF
+    doc.save('qrcodes.pdf');
   }
+  
 
   back(): void {
-    console.log("back: ")
+    console.log("back: ");
     this.ngZone.run(() => {
       this.router.navigate(['/admin']);
     });
